631dc3b67f6bdafb4f0948c22e4fee43cac603c1
Alex Hoyau <a.hoyau@silexlabs.org>
2020-10-23 20:35:22 +0200

    jekyll

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-qui","displayName":"qui","link":{"linkType":"LinkTypePage","href":"#!page-qui"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-comment","displayName":"comment","link":{"linkType":"LinkTypePage","href":"#!page-comment"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-r-alisations","displayName":"réalisations","link":{"linkType":"LinkTypePage","href":"#!page-r-alisations"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
