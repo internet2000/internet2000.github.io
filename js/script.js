1d0a795598e5d60bc313e781ec5826c579b2e76b
Moly <64364914+molyrichez@users.noreply.github.com>
2021-02-26 19:13:55 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(function(){
             $('.borderscroll').removeClass('border');
         }
     });
-    /*blabla
+
         var elements = $('.jarallax').jarallax({
          speed: 0.5/silex.scale,
         //onInit: function(){ console.log('onInit', this)},
@@ -24,5 +24,64 @@ $(function(){
                 })
             }
         }
-        */
+        })
+    var initialMinHeight = $('.hero-section').height()
+    function unscale() {
+        $('.hero-section').css({
+            minHeight: initialMinHeight / silex.scale,
+        })
+    }
+    $(window).on('resize', unscale)
+    unscale()
+
 });
+
+/*
+animation widget from here: https://github.com/silexlabs/Silex/issues/443
+*/
+$(function() {
+            var win = $(window)
+            var size = {
+                h: win.height(),
+                w: win.width()
+            }
+            var initial = {
+                h: size.h / silex.scale,
+                w: size.w / silex.scale
+            }
+            var offset = {
+                h: initial.h - size.h,
+                w: initial.w - size.w,
+            }
+            // this function is used to apply an offset to the detection of the active boundaries of the viewport
+            // this is required when the viewport width is smaller than the website (when Silex scales down the website)
+            function getOffset(el) {
+                var position = $(el).position()
+                var localOffset = {
+                    h: offset.h + (position.top * (1-silex.scale)),
+                    w: offset.w + (position.left * (1-silex.scale)),
+                }
+                return {
+                    top: offset.h,
+                    bottom: -offset.h,
+                    left: offset.w,
+                    right: -offset.w,
+                }
+            }
+            var sr = ScrollReveal({
+                distance: '100px',
+                reset: false,
+            });
+            $('.from-left').each(function() {
+                sr.reveal(this, { origin:  'left', viewOffset: getOffset(this)});
+            })
+            $('.from-right').each(function() {
+                sr.reveal(this, { origin:  'right', viewOffset: getOffset(this)});
+            })
+            $('.from-top').each(function() {
+                sr.reveal(this, { origin:  'top', viewOffset: getOffset(this)});
+            })
+            $('.from-bottom').each(function() {
+                sr.reveal(this, { origin:  'bottom', viewOffset: getOffset(this)});
+            })
+})
