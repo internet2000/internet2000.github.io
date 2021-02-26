70cc4df653897072722595c20b9b67fecd6d5d59
Moly <64364914+molyrichez@users.noreply.github.com>
2021-02-26 18:48:06 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -4,20 +4,28 @@
     window.silex.data = {"site":{"width":1280},"pages":[{"id":"page-home","displayName":"home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-404","displayName":"404","link":{"linkType":"LinkTypePage","href":"#!page-404"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-merci","displayName":"merci","link":{"linkType":"LinkTypePage","href":"#!page-merci"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
 $(function(){
     $(window).scroll(function(){
-        if($(this).scrollTop()>0){
+        if($(this).scrollTop()>36){
             $('.borderscroll').addClass('border');
         } else {
             $('.borderscroll').removeClass('border');
         }
     });
-    $('.jarallax')
-    .css({
-        width: (1/silex.scale * 100) + '%',
-        height: (1/silex.scale * 100) + '%',
-    })
-    .jarallax({
-        speed: 0.5/silex.scale,
-    });
+        var elements = $('.jarallax').jarallax({
+         speed: 0.5/silex.scale,
+        //onInit: function(){ console.log('onInit', this)},
+        onCoverImage: function(){
+            var container = $('#jarallax-container-' + this.instanceID).children()[0]
+            if (container) {
+                container = $(container)
+                container.css({
+                    'width': (container.width() / silex.scale) + 'px',
+                    'height': (container.height() / silex.scale) + 'px',
+                })
+            }
+        }
+});
+
+
 $('.fade-up').attr('data-aos', 'fade-up')
 $('.fade-down').attr('data-aos', 'fade-down')
 $('.fade-left').attr('data-aos', 'fade-left')
